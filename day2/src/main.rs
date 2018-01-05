74ed8ab453f95d0e1a226fc5b40261a81e1c0955
Sebastian Redl <s.redl@kalbeck.com>
2018-01-05 13:42:01 +0100

    Solve day 2 part 2.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -10,11 +10,34 @@ fn get_input() -> Vec<Vec<u32>> {
     result
 }
 
-fn solve(sheet: &[Vec<u32>]) -> u32 {
-    sheet.iter().map(|line| line.iter().max().unwrap() - line.iter().min().unwrap()).fold(0, |sum, i| sum + i)
+fn sort_lines(data: &mut [Vec<u32>]) {
+    for v in data {
+        v.sort_unstable_by(|a, b| b.cmp(a));
+    }
+}
+
+fn solve1(sheet: &[Vec<u32>]) -> u32 {
+    sheet.iter().map(|line| line.first().unwrap() - line.last().unwrap()).fold(0, |sum, i| sum + i)
+}
+
+fn solve2(sheet: &[Vec<u32>]) -> u32 {
+    sheet.iter().map(|line| even_division(line)).fold(0, |sum, i| sum + i)
+}
+
+fn even_division(line: &[u32]) -> u32 {
+    for i in 0..line.len()-1 {
+        for j in i+1..line.len() {
+            if line[i] % line[j] == 0 {
+                return line[i] / line[j];
+            }
+        }
+    }
+    panic!("line didn't have evenly divisible pair");
 }
 
 fn main() {
-    let data = get_input();
-    println!("Solution: {}", solve(&data));
+    let mut data = get_input();
+    sort_lines(&mut data);
+    println!("Solution Part A: {}", solve1(&data));
+    println!("Solution Part B: {}", solve2(&data));
 }
