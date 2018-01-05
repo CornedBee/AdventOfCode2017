e8912cedfe98e5b99142d5de8c97b079456b6db8
Sebastian Redl <s.redl@kalbeck.com>
2018-01-05 13:30:16 +0100

    Solve day 1 part 2.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -6,13 +6,14 @@ fn get_input() -> Vec<u32> {
     data.chars().flat_map(|c| c.to_digit(10)).collect()
 }
 
-fn solve(numbers: &[u32]) -> u32 {
-    numbers.iter().zip(numbers.iter().cycle().skip(1))
+fn solve(numbers: &[u32], skip: usize) -> u32 {
+    numbers.iter().zip(numbers.iter().cycle().skip(skip))
         .filter(|&(a, b)| a == b)
         .fold(0, |sum, (a, _)| sum + a)
 }
 
 fn main() {
     let numbers = get_input();
-    println!("Solution: {}", solve(&numbers));
+    println!("Solution Part A: {}", solve(&numbers, 1));
+    println!("Solution Part B: {}", solve(&numbers, numbers.len() / 2));
 }
