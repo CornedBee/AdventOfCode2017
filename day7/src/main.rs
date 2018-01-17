7d599b7b06a87c829b0819cef46d24085972cd84
Sebastian Redl <s.redl@kalbeck.com>
2018-01-17 15:39:06 +0100

    Solve day 7 part 2.

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,11 +1,13 @@
 #[macro_use] extern crate scan_rules;
 
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::io;
 use scan_rules::scanner::Word;
 
 struct Program {
     name: String,
+    weight: i32,
     children: Vec<String>,
 }
 
@@ -16,8 +18,8 @@ fn read_program() -> Result<Option<Program>, scan_rules::ScanError> {
         Ok(0) => Ok(None),
         Ok(_) =>
             Ok(Some(scan!(&line;
-                (let name: Word<String>, "(", let _: u32, ")", ["->", [ let children : Word<String> ],+ ]?) =>
-                    Program { name, children: children.into_iter().next().unwrap_or(vec![]) },
+                (let name: Word<String>, "(", let weight: i32, ")", ["->", [ let children : Word<String> ],+ ]?) =>
+                    Program { name, weight, children: children.into_iter().next().unwrap_or(vec![]) },
             )?)),
     }
 }
@@ -33,7 +35,7 @@ fn get_input() -> Vec<Program> {
     result
 }
 
-fn solve(data: &[Program]) -> &str {
+fn solve1(data: &[Program]) -> &str {
     let programs : HashSet<&str> = data.iter().map(|p| p.name.as_str()).collect();
     let children : HashSet<&str> = data.iter().flat_map(|p| p.children.iter().map(|c| c.as_str())).collect();
 
@@ -45,8 +47,50 @@ fn solve(data: &[Program]) -> &str {
     root
 }
 
+enum FindImbalanceResult {
+    FoundImbalance(i32),
+    CumulativeWeight(i32),
+}
+
+fn solve2(data: &[Program], root: &str) -> i32 {
+    let programs = data.iter().map(|p| (p.name.as_str(), p)).collect();
+    match find_imbalance(&programs, root) {
+        FindImbalanceResult::FoundImbalance(imbalance) => imbalance,
+        FindImbalanceResult::CumulativeWeight(_) => panic!("no imbalance found")
+    }
+}
+
+fn find_imbalance(programs: &HashMap<&str, &Program>, node: &str) -> FindImbalanceResult {
+    let program = programs[node];
+    let mut cumulative_weight = program.weight;
+    let mut weights = HashMap::<i32, Vec<&str>>::new();
+    for c in &program.children {
+        let weight = match find_imbalance(programs, &c) {
+            FindImbalanceResult::CumulativeWeight(w) => w,
+            r => return r,
+        };
+        cumulative_weight += weight;
+        weights.entry(weight).or_insert_with(|| vec![]).push(c);
+    }
+
+    if weights.len() <= 1 {
+        return FindImbalanceResult::CumulativeWeight(cumulative_weight);
+    }
+
+    let odd = weights.iter().find(|e| e.1.len() == 1).expect("can't find lone entry");
+    let odd_weight = odd.0;
+    let odd_name = odd.1[0];
+    let expected_weight = weights.iter().find(|e| e.1.len() != 1).expect("can't find standard entry").0;
+    let adjustment = expected_weight - odd_weight;
+    let odd_own_weight = programs[odd_name].weight;
+
+    FindImbalanceResult::FoundImbalance(odd_own_weight + adjustment)
+}
+
 fn main() {
     let data = get_input();
-    let result = solve(&data);
-    println!("Solution: {}", result);
+    let result = solve1(&data);
+    println!("Solution 1: {}", result);
+    let result = solve2(&data, result);
+    println!("Solution 2: {}", result);
 }
